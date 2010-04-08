2170e040f43ae8f2da6c6e4c1389f5676082c9e5
Caius Durling <dev@caius.name>
2010-04-08 01:55:00 +0100

    Amending the spec to let you set a custom string as anchor text, or pass a function to figure it out at run-time

--- spec/unit/spec.js
+++ spec/unit/spec.js
@@ -1,10 +1,11 @@
-
 describe 'showLink()'
 
   before_each
     html = $(fixture("fieldset"))
     fieldset = html.find("fieldset")
-    fieldset.showLink();
+    fieldset.showLink(function() {
+      return "anchor text";
+    });
 
     anchor = html.find("a.show-next-element")
   end
@@ -17,13 +18,34 @@ describe 'showLink()'
     html.should.have_tag("a.show-next-element")
   end
 
-  it 'should insert the anchor just before the fieldset'
+  it 'should insert the anchor just before the specified element'
     anchor.next().attr("tagName").toLowerCase().should.be "fieldset"
   end
 
-  it 'should insert the anchor with the text of the fieldset\'s legend'
-    anchor_text = html.find("fieldset legend").text()
-    anchor.text().should.be anchor_text
+  it 'should accept a function returning a string'
+    anchor.text().should.be "anchor text"
+  end
+
+  it 'should accept a function hitting the DOM to return a string'
+    html2 = $(fixture("table"))
+    table = html2.find("table")
+
+    table.showLink(function() {
+      return $(this).attr("title");
+    })
+
+    a2 = html2.find("a.show-next-element")
+    a2.text().should.be "table header"
+  end
+
+  it 'should use a string argument as the anchor text'
+    html2 = $(fixture("table"))
+    table = html2.find("table")
+    
+    table.showLink("Show Me")
+    
+    a2 = html2.find("a.show-next-element")
+    a2.text().should.be "Show Me"
   end
 
   describe 'clicking the link'
